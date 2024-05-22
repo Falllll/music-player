021e786b64273655538adb781de77f71da87371f
Falllll <falldyshinoda@gmail.com>
2024-05-22 15:48:36 +0700

    pause song

--- js/script.js
+++ js/script.js
@@ -102,6 +102,13 @@ const playSong = (id) => {
     audio.play();
 };
 
+const pauseSong = () => {
+    userData.songCurrentTime = audio.currentTime;
+    
+    playButton.classList.remove("playing");
+    audio.pause();
+};
+
 const renderSongs = (array) => {
     const songsHTML = array
         .map((song) => {
@@ -131,6 +138,7 @@ playButton.addEventListener("click", () => {
         playSong(userData?.currentSong.id);
     }
 });
+pauseButton.addEventListener('click', pauseSong);
 
 const sortSongs = () => {
     userData?.songs.sort((a, b) => {
